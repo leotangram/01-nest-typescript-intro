979db2cd4f0afa5933d297fcc471b2f268e4acfe
Leonardo Omaña <leotangram@gmail.com>
2023-07-17 20:51:17 -0500

    getters, methods, this

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,5 +1,23 @@
 export class Pokemon {
-  constructor(public readonly id: number, public name: string) {}
+  get imageUrl(): string {
+    return `https://pokemon.com/${this.id}.jpg`
+  }
+
+  constructor(
+    public readonly id: number,
+    public name: string // public imageUrl: string
+  ) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`)
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name}`)
+  }
 }
 
 export const charmander = new Pokemon(4, 'Charmander')
+
+charmander.speak()
+charmander.scream()
