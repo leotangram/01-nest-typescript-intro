ad8b91d1bd25852b0f14faa20b9eb549a7887056
Leonardo Omaña <leotangram@gmail.com>
2023-07-18 14:40:50 -0500

    Add data type to response http

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import { Move, PokeAPIResponse } from '../interfaces/pokeapi-response.interface'
+
 export class Pokemon {
   get imageUrl(): string {
     return `https://pokemon.com/${this.id}.jpg`
@@ -15,9 +18,19 @@ export class Pokemon {
   speak() {
     console.log(`${this.name}, ${this.name}`)
   }
+
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeAPIResponse>(
+      'https://pokeapi.co/api/v2/pokemon/4'
+    )
+
+    return data.moves
+  }
 }
 
 export const charmander = new Pokemon(4, 'Charmander')
 
-charmander.speak()
-charmander.scream()
+// charmander.speak()
+// charmander.scream()
+
+charmander.getMoves()
